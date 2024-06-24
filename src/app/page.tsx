13b3aec5cd843d00aa17af5862a3d2ac65a520fb
satya740 <satyaprakash74029@gmail.com>
2024-06-25 00:35:58 +0530

    set-up

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
+        <div>
+            <p>Satya Prakash !!</p>
+        </div>
+    </MaxWidthWrapper>
+  );
+}
