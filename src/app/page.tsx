dfbf70e2aec81408f52c5d3d5fb3be77c4f6e133
satya740 <satyaprakash74029@gmail.com>
2024-08-18 14:56:26 +0530

    adding-pictures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,13 @@ export default function Home() {
     <div className="bg-slate-50">
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
-            <div></div>
+            <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+                <div className="relative max-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+                  <div className="absolute w-28 -top-20 hidden lg:block">
+
+                  </div>
+                </div>
+            </div>
         </MaxWidthWrapper>
       </section>
     </div>
