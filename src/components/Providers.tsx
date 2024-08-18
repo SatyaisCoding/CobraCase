94c1366155af32c45b821660e77f5e91b3f6e6f9
satya740 <satyaprakash74029@gmail.com>
2024-08-18 15:34:03 +0530

    auth

---
+++ src/components/Providers.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+// import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactNode } from 'react'
+
+// const client = new QueryClient()
+
+const Providers = ({ children }: { children: ReactNode }) => {
+//   return 
+return <div>Satyaa</div>
+}
+
+export default Providers
